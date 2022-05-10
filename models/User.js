a507e93e57d0ad36d72456cfeb3d0f3d303e178a
wilker <wilker@iqiglobal.com>
2022-05-11 02:03:38 +0800

    Update required type

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   userId: {
     type: String,
+    required: [ true, 'Please add a user id' ],
     unique: true
   },
   login: {
