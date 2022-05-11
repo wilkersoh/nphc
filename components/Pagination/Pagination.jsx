d731bcdb91e6bf13ef29599ef41fc4e62c500c7d
wilker <wilker@iqiglobal.com>
2022-05-12 01:07:50 +0800

    fixed ui

--- components/Pagination/Pagination.jsx
+++ components/Pagination/Pagination.jsx
@@ -4,7 +4,7 @@ import "rc-pagination/assets/index.css";
 
 const Pagination = ({ countPerPage, updatePage, currentPage, totalPage }) => {
   return (
-    <div className='mt-8'>
+    <div className='mt-8 pb-10'>
       <RcPagination
         pageSize={ countPerPage }
         onChange={ updatePage }
